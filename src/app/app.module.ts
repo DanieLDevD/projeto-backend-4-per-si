62de1bfa1c0b6ae2677c17e8b30e8d6a79d5b3ac
Victor-Prudente <victoreduprudente@gmail.com>
2023-10-07 23:10:27 -0400

    Authentication and guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '../database/database.module';
 import { UserModule } from 'src/modules/user/user.module';
 import { TaskModule } from 'src/modules/task/task.module';
+import { AuthModule } from 'src/modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TaskModule } from 'src/modules/task/task.module';
     DatabaseModule.forRoot(),
     UserModule,
     TaskModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
